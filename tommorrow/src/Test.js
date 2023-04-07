28eadf69306adceb326ffff78563a0f8c01f2420
DaBin_KiM <rhkahrake100@naver.com>
2023-04-07 14:23:12 +0900

    Test.js 수정함

--- tommorrow/src/Test.js
+++ tommorrow/src/Test.js
@@ -5,7 +5,7 @@ function Test() {
   const [Temp, setTemp] = useState(0);
   return (
     <div>
-      <h1 className='test'> 김다빈 branch2입니다.  </h1>
+      <h1 className='test'> 브랜치 및 커밋 테스트 중입니다.   </h1>
       {Temp}
       <button onClick={function () { setTemp(Temp + 3) }}>증가</button>
     </div>
